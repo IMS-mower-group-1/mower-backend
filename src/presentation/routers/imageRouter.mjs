1685bff05a8b4ebe7f091dd1a852cbd524172cdd
philip <philleabraham@hotmail.com>
2023-04-02 19:48:25 +0200

    Implemented the Dependency injection in the layers moved index.mjs to root. Start the app by running node index.mjs from root

--- src/presentation/routers/imageRouter.mjs
+++ src/presentation/routers/imageRouter.mjs
@@ -14,59 +14,64 @@ const __dirname = path.dirname(__filename);
 
 console.log(__dirname);
 
-const client = new ImageAnnotatorClient({
-  keyFilename: __dirname + "/../config/tgin13-22678df230b0.json",
-});
+export default function createImageRouter({imageService}){
+  
+  const client = new ImageAnnotatorClient({
+    keyFilename: __dirname + "/../config/tgin13-22678df230b0.json",
+  });
 
-const router = express.Router();
+  const router = express.Router();
 
-router.use(bodyParser.raw({ type: "application/octet-stream", limit: "10mb" }));
+  router.use(bodyParser.raw({ type: "application/octet-stream", limit: "10mb" }));
 
-// Get image data
-router.get("/:id", (req, res) => {});
+  // Get image data
+  router.get("/:id", (req, res) => {});
 
-// Upload image
-// Find out how IMAGE can be sent from Mower to API?
-router.post("/upload/:mowerID", async (req, res) => {
-  // 1. Upload to Firebase Storage
-  const dummyUserID = req.params.mowerID;
-  const date = "2022-03-30_13:35:24";
-  const storage = getStorage();
+  // Upload image
+  // Find out how IMAGE can be sent from Mower to API?
+  router.post("/upload/:mowerID", async (req, res) => {
+    // 1. Upload to Firebase Storage
+    const dummyUserID = req.params.mowerID;
+    const date = "2022-03-30_13:35:24";
+    const storage = getStorage();
 
-  const uint8Array = new Uint8Array(req.body);
+    const uint8Array = new Uint8Array(req.body);
 
-  const newImageReference = ref(storage, `${dummyUserID}/${date}.jpg`);
+    const newImageReference = ref(storage, `${dummyUserID}/${date}.jpg`);
 
-  try {
-    await uploadBytes(newImageReference, uint8Array)
-    console.log(`Uploaded Image!`);
+    try {
+      await uploadBytes(newImageReference, uint8Array)
+      console.log(`Uploaded Image!`);
 
-    // 2. Perform Image Classification using Google API
+      // 2. Perform Image Classification using Google API
 
-    const features = [
-      {
-        type: "OBJECT_LOCALIZATION",
-        maxResults: 1,
-      },
-    ];
+      const features = [
+        {
+          type: "OBJECT_LOCALIZATION",
+          maxResults: 1,
+        },
+      ];
 
-    const request = {
-      image: {
-        content: Buffer.from(uint8Array).toString("base64"),
-      },
-      features: features,
-    };
+      const request = {
+        image: {
+          content: Buffer.from(uint8Array).toString("base64"),
+        },
+        features: features,
+      };
 
-    const results = await client.annotateImage(request)
-    console.log(results[0].localizedObjectAnnotations);
+      const results = await client.annotateImage(request)
+      console.log(results[0].localizedObjectAnnotations);
 
-    // 3. Add item to Firestore, connecting both image and annotations.
+      // 3. Add item to Firestore, connecting both image and annotations.
 
-    res.sendStatus(200);
+      res.sendStatus(200);
 
-  } catch {
-    res.sendStatus(500);
-  }
-});
+    } catch {
+      res.sendStatus(500);
+    }
+  });
 
-export default router;
+  return router
+
+}
+
